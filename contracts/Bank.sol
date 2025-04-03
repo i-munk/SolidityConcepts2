6cae3923322dfce4b65d820eae6aeed5857ca4d6
i-munk <oceandata0@gmail.com>
2025-04-03 14:16:08 +0900

    solidity Vault&Bank

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -1,1 +1,20 @@
-contract Bank {}
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "./Vault.sol";
+
+contract Bank is Vault {
+    event Withdrawn(address indexed user, uint256 amount);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call withdraw.");
+        _;
+    }
+
+    function withdraw(uint256 amount) public onlyOwner {
+        require(amount <= sentValue, "Insufficient balance in Vault.");
+        sentValue -= amount;
+        payable(msg.sender).transfer(amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+}
--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -1,1 +1,59 @@
-contract Vault {}
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Vault {
+    address public owner;
+    uint256 public sentValue;
+    uint256 public timestamp;
+    uint256 public gasUsed;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function deposit() public payable {
+        require(msg.value > 0, "Must send some ether.");
+        sentValue = msg.value;
+        timestamp = block.timestamp;
+    }
+
+    function getCaller() public view returns (address) {
+        return msg.sender;
+    }
+
+    function getOrigin() public view returns (address) {
+        return tx.origin;
+    }
+
+    function getBlockDetails()
+        public
+        view
+        returns (
+            uint256 blockNumber,
+            uint256 blockPrevrandao,
+            uint256 blockGasLimit,
+            address blockCoinbase
+        )
+    {
+        return (
+            block.number,
+            block.prevrandao,
+            block.gaslimit,
+            block.coinbase
+        );
+    }
+
+    function trackGasUsage() public {
+        uint256 initialGas = gasleft();
+        uint256 dummy; // simple operation to consume gas
+        for (uint256 i = 0; i < 10; i++) {
+            dummy += i;
+        }
+        uint256 finalGas = gasleft();
+        gasUsed = initialGas - finalGas;
+    }
+
+    function generateHash(string memory input) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(input));
+    }
+}
